a4bdefaef5bc98284e4517aa9ee73c1e36f5ca8d
Ashley Trinh <ashqtrinh@gmail.com>
2018-04-24 13:02:30 -0700

    Add AM/PM indicators

--- sections/time.zsh
+++ sections/time.zsh
@@ -8,11 +8,15 @@
 # ------------------------------------------------------------------------------
 
 SPACESHIP_TIME_SHOW="${SPACESHIP_TIME_SHOW=false}"
+SPACESHIP_TIME_AMPM_SHOW="${SPACESHIP_TIME_AMPM_SHOW=false}"
 SPACESHIP_TIME_PREFIX="${SPACESHIP_TIME_PREFIX="at "}"
 SPACESHIP_TIME_SUFFIX="${SPACESHIP_TIME_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
 SPACESHIP_TIME_FORMAT="${SPACESHIP_TIME_FORMAT=false}"
 SPACESHIP_TIME_12HR="${SPACESHIP_TIME_12HR=false}"
 SPACESHIP_TIME_COLOR="${SPACESHIP_TIME_COLOR="yellow"}"
+SPACESHIP_TIME_PM="${SPACESHIP_TIME_PM="🌙 "}"
+SPACESHIP_TIME_AM="${SPACESHIP_TIME_AM="⛅ "}"
+
 
 # ------------------------------------------------------------------------------
 # Section
@@ -21,14 +25,25 @@ SPACESHIP_TIME_COLOR="${SPACESHIP_TIME_COLOR="yellow"}"
 spaceship_time() {
   [[ $SPACESHIP_TIME_SHOW == false ]] && return
 
+  local day_phase="$(print -P "%D{%p}")"
+  local day_symbol
+
+  if [[ day_phase == "AM" ]]; then
+    day_symbol="${SPACESHIP_TIME_AM}"
+  else
+    day_symbol="${SPACESHIP_TIME_PM}"
+  fi
+
+  [[ $SPACESHIP_TIME_SHOW_AMPM == false ]] && day_symbol=""
+
   local time_str
 
   if [[ $SPACESHIP_TIME_FORMAT != false ]]; then
-    time_str="${SPACESHIP_TIME_FORMAT}"
+    time_str="$day_symbol${SPACESHIP_TIME_FORMAT}"
   elif [[ $SPACESHIP_TIME_12HR == true ]]; then
-    time_str="%D{%r}"
+    time_str="$day_symbol%D{%r}"
   else
-    time_str="%D{%T}"
+    time_str="$day_symbol%D{%T}"
   fi
 
   spaceship::section \
